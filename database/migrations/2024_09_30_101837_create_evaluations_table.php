a95619bd5acd25b7b19ffc2da4053a61f5c7d352
sofianghobrini <sofiangho31000@outlook.fr>
2024-11-18 10:46:32 +0100

    rename

--- database/migrations/2024_09_30_101837_create_evaluations_table.php
+++ database/migrations/2024_09_30_101837_create_evaluations_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('module_id')->constrained('modules'); // Relation avec les modules
+            $table->unsignedBigInteger('eleve_id');
+            $table->foreignId('eleve_id')->constrained('eleves')->onDelete('cascade');
             $table->date('date');         // Date de l'évaluation
             $table->string('titre');      // Titre de l'évaluation
             $table->float('coefficient'); // Coefficient de l'évaluation
