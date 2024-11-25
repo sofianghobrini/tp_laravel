75709088e2aef5cba5199db0f9e3144a6dfa49f4
sofianghobrini <sofiangho31000@outlook.fr>
2024-11-25 11:53:55 +0100

    id inutile

--- database/migrations/2024_09_30_101837_create_evaluations_table.php
+++ database/migrations/2024_09_30_101837_create_evaluations_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('evaluations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('module_id')->constrained('modules'); // Relation avec les modules
             $table->unsignedBigInteger('eleve_id');
             $table->foreignId('eleve_id')->constrained('eleves')->onDelete('cascade');
             $table->date('date');         // Date de l'évaluation
