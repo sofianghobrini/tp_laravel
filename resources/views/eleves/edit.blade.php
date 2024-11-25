cb35bd0ed8a91f88c6031480f9b3c7f939770fdb
sofianghobrini <sofiangho31000@outlook.fr>
2024-11-25 11:24:06 +0100

    probleme d'initialisation

--- resources/views/eleves/edit.blade.php
+++ resources/views/eleves/edit.blade.php
@@ -18,16 +18,16 @@
         @csrf
         @method('PUT')
         <div class="mb-3">
-            <label for="nom" class="form-label">Nom :</label>
-            <input type="text" id="nom" name="nom" class="form-control" value="{{ old('nom', $eleve->nom) }}" required>
+            <label for="name" class="form-label">Nom :</label>
+            <input type="text" id="name" name="name" class="form-control" value="{{ old('name', $eleve->name) }}" required>
         </div>
         <div class="mb-3">
-            <label for="prenom" class="form-label">Prénom :</label>
-            <input type="text" id="prenom" name="prenom" class="form-control" value="{{ old('prenom', $eleve->prenom) }}" required>
+            <label for="prénom" class="form-label">Prénom :</label>
+            <input type="text" id="prénom" name="prénom" class="form-control" value="{{ old('prénom', $eleve->prénom) }}" required>
         </div>
         <div class="mb-3">
-            <label for="dateNaissance" class="form-label">Date de naissance :</label>
-            <input type="date" id="dateNaissance" name="dateNaissance" class="form-control" value="{{ old('dateNaissance', $eleve->dateNaissance) }}" required>
+            <label for="date_naissance" class="form-label">Date de naissance :</label>
+            <input type="date" id="date_naissance" name="date_naissance" class="form-control" value="{{ old('date_naissance', $eleve->date_naissance) }}" required>
         </div>
         <div class="mb-3">
             <label for="numéro_étudiant" class="form-label">Numéro étudiant :</label>
--- resources/views/eleves/show.blade.php
+++ resources/views/eleves/show.blade.php
@@ -4,9 +4,9 @@
 <div class="container mt-4">
     <h1 class="mb-4">Détails de l'élève</h1>
     <ul class="list-group">
-        <li class="list-group-item"><strong>Nom :</strong> {{ $eleve->nom }}</li>
-        <li class="list-group-item"><strong>Prénom :</strong> {{ $eleve->prenom }}</li>
-        <li class="list-group-item"><strong>Date de naissance :</strong> {{ $eleve->dateNaissance }}</li>
+        <li class="list-group-item"><strong>Nom :</strong> {{ $eleve->name }}</li>
+        <li class="list-group-item"><strong>Prénom :</strong> {{ $eleve->prénom }}</li>
+        <li class="list-group-item"><strong>Date de naissance :</strong> {{ $eleve->date_naissance }}</li>
         <li class="list-group-item"><strong>Numéro étudiant :</strong> {{ $eleve->numéro_étudiant }}</li>
         <li class="list-group-item"><strong>Email :</strong> {{ $eleve->email }}</li>
     </ul>
